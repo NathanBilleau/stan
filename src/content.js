04920d9b0e95ff166bdac7bc0cdd7ea9f503b0fd
NathanBilleau <nbilleau@gmail.com>
2020-05-31 15:40:57 +0200

    select

---
+++ src/content.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Les débuts du rap US',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur inventore minus reprehenderit amet aliquid sunt eius possimus suscipit veritatis, maxime neque vel dolorem ipsum quo totam doloremque dolores! Aliquid, vel?',
+        video: 'https://www.youtube.com/watch?v=PrqDFDEJMmU',
+        picture: 'https://images.unsplash.com/photo-1479405260053-0450545c9a3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80'
+    },
+    {
+        title: 'Les débuts du rap FR',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur inventore minus reprehenderit amet aliquid sunt eius possimus suscipit veritatis, maxime neque vel dolorem ipsum quo totam doloremque dolores! Aliquid, vel?',
+        video: 'https://www.youtube.com/watch?v=PrqDFDEJMmU',
+        picture: 'https://images.unsplash.com/photo-1479405260053-0450545c9a3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80'
+    },
+    {
+        title: 'Les débuts du rap UK',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur inventore minus reprehenderit amet aliquid sunt eius possimus suscipit veritatis, maxime neque vel dolorem ipsum quo totam doloremque dolores! Aliquid, vel?',
+        video: 'https://www.youtube.com/watch?v=PrqDFDEJMmU',
+        picture: 'https://images.unsplash.com/photo-1479405260053-0450545c9a3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80'
+    }
+]
--- src/pages/playlist.svelte
+++ src/pages/playlist.svelte
@@ -8,6 +8,30 @@
     import StartPlaylist from '../components/StartPlaylist.svelte'
 
     import SideText from '../components/SideText.svelte'
+
+    import content from '../content'
+
+    console.log(content);
+    
+
+    let index = 0
+    let selectedItems = []
+
+    function backward() {
+        if(index == 0) index = content.length
+        index--
+    }
+
+    function add() {
+        selectedItems.push(index)
+    }
+
+    function forward() {
+        index++
+        if(index == content.length) index = 0
+    }
+
+
 </script>
 
 <main>
@@ -19,6 +43,26 @@
 
     <SideText text="l'histoire du rap" />
 
+    <div class="covers">
+        {#each content as item, i}
+            <img class={index == i ? "selected" : ''} src={item.picture} alt={item.title}>
+        {/each}
+        <div class="controls">
+            <button on:click={backward}><i class="fa fa-backward"></i></button>
+            {#if selectedItems.indexOf(index) !== -1}
+                    <Selected />
+            {:else}
+                    <button on:click={add}><i class="fa fa-plus"></i></button>
+            {/if}
+            <!-- <button on:click={add}><i class="fa fa-plus"></i></button> -->
+            <button on:click={forward}><i class="fa fa-forward"></i></button>
+        </div>
+    </div>
+
+    <h1>{content[index].title}</h1>
+    <p>{content[index].description}</p>
+
+    <StartPlaylist />
 </main>
 
 <style>
@@ -28,4 +72,67 @@ main {
     background-blend-mode: multiply;
 }
 
+.covers {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    height: 48vh;
+    width: 48vh;
+}
+
+.covers img {
+    width: 40vh;
+    height: 40vh;
+    object-fit: cover;
+    filter: saturate(0);
+    position: absolute;
+    transition: .2s;
+}
+
+/* previous */
+.covers img {
+    transform: translate(-70px);
+}
+
+/* next */
+.covers .selected + img {
+    transform: translate(70px);
+}
+
+/* selected */
+.covers img.selected {
+    transform: scale(1.2);
+    z-index: 999;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .5);
+}
+
+h1,
+p {
+    text-align: center;
+    color: #fff;
+    max-width: 60vw;
+}
+
+.controls {
+    position: absolute;
+    z-index: 9999;
+    bottom: 0;
+    width: 100%;
+    background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, .7));
+    display: flex;
+    justify-content: center;
+    
+}
+
+.controls button{
+    background: none;
+    border: none;
+}
+
+.controls i {
+    color: #fff;
+    cursor: pointer;
+    font-size: 1.3em;
+}
 </style>
